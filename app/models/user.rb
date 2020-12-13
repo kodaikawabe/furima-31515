73e0e0d36bcfe67fe5ad07181c480670870b1f4b
kodaikawabe <kokodai5124@icloud.com>
2020-12-13 20:48:47 +0900

    バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :firstname, presence: true
+  validates :family_name, presence: true
+  validates :first_name_kana, presence: true
+  validates :family_name_kana, presence: true
+  validates :birthday, presence: true
+
+  has_many :itemns, dependent: :destroy
+  has_many :buys, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
 end
