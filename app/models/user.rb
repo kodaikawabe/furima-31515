aaea48220b6f41a8e2c871785ddd49482d569722
kodaikawabe <kokodai5124@icloud.com>
2020-12-13 21:53:49 +0900

    ユーザー登録機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :firstname, presence: true
+  validates :first_name, presence: true
   validates :family_name, presence: true
   validates :first_name_kana, presence: true
   validates :family_name_kana, presence: true
