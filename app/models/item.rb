9343e470218b7eb82a1e3918bf255588d946cca3
kodaikawabe <kokodai5124@icloud.com>
2021-01-04 19:46:00 +0900

    バリデーション編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,16 @@
 class Item < ApplicationRecord
-  def new
-    @item = Item.new
-  end
+  belongs_to :user
+  has_many :comments
+  has_one :buy
+  has_one_attached :image
 
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :status_id
+    validates :shipping_charges_id
+    validates :area_id 
+    validates :date_of_shipment_id
+    validates :item_info
+  end
 end
