9f18b5fbf3adf6f49a7e118f5d35e9878182f53b
kodaikawabe <kokodai5124@icloud.com>
2021-01-09 11:48:56 +0900

    ActiveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,14 +3,25 @@ class Item < ApplicationRecord
   has_many :comments
   has_one :buy
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+  belongs_to :status
+  belongs_to :date_of_shipment
+  belongs_to :shipping_charges
+  belongs_to :area
 
   with_options presence: true do
     validates :name
     validates :price
+    validates :item_info
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
     validates :status_id
     validates :shipping_charges_id
     validates :area_id 
     validates :date_of_shipment_id
-    validates :item_info
   end
+
 end
