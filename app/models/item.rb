7d6b17efb47988c48c557fc1b41da670f7539fc0
kodaikawabe <kokodai5124@icloud.com>
2021-01-13 14:38:46 +0900

    修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,14 +11,13 @@ class Item < ApplicationRecord
   belongs_to :area
 
   with_options presence: true do
-    validates :image, presence: true
+    validates :image
     validates :name
     validates :item_info
+    validates :price
+    validates :price, numericality: { only_integer: true, message: 'Half-width number' }
+    validates_inclusion_of :price, in: 300..9_999_999, message: 'Out of setting range'
   end
-
-  validates :price, presence: true
-  validates :price, numericality: { only_integer: true, message: 'Half-width number' }
-  validates_inclusion_of :price, in: 300..9_999_999, message: 'Out of setting range'
 
   with_options numericality: { other_than: 1, message: 'Select' } do
     validates :category_id
