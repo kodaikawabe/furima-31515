780cf5a147918abc1c3623274d58b315d7070e55
kodaikawabe <kokodai5124@icloud.com>
2021-01-21 14:36:40 +0900

    アソシエーション記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :comments
-  has_one :buy
-  has_one_attached :image
+  has_many :comments, dependent: :destroy
+  has_one :buy, dependent: :destroy
+  has_one_attached :image, dependent: :destroy
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
