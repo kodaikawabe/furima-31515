0314c89920f45554b6fde79d1da540f7e32b726a
kodaikawabe <kokodai5124@icloud.com>
2021-01-21 14:56:42 +0900

    buysコントローラー編集

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,2 +1,15 @@
 class BuysController < ApplicationController
+
+  def index
+    @usre_orser = UserOrder.new(buys_params)
+  end
+
+  def create
+  end
+
+  private
+  def buys_params
+    params.require(:user_order).permit(:postal_code, :prefectures_id, :municipal_district,:address,
+      :building_name, :phone_number, :user_id,:item_id, :token)
+ end
 end
