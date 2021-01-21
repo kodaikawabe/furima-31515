4e86579492e65ff751431b934385b36637f3b031
kodaikawabe <kokodai5124@icloud.com>
2021-01-21 14:29:40 +0900

    adressesテーブル作成

---
+++ db/migrate/20210121052439_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string     :postal_code,        null: false
+      t.integer    :prefectures_id,     null: false
+      t.string     :municipal_district, null: false
+      t.string     :address,            null:  false
+      t.string     :building_name,      null: false
+      t.string     :phone_number,       null: false
+      t.references :buy,                foreign_key: true
+      t.timestamps
+    end
+  end
+end
