a42c735dbab52a3b74a25f1f7bfd2694676b269a
kodaikawabe <kokodai5124@icloud.com>
2021-01-22 19:49:42 +0900

    コントローラー編集

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,15 +1,24 @@
 class BuysController < ApplicationController
 
   def index
-    @user_order = UserOrder.new(buy_params)
+    @user_order = UserOrder.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @user_order = UserOrder.new(buy_params)
+    if @user_order.valid?
+      @user_order.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
   end
 
   private
   def buy_params
     params.require(:user_order).permit(:postal_code, :prefectures_id, :municipal_district,:address,
-      :building_name, :phone_number, :user, :item)
+      :building_name).merge( user_id: current_user.id)
  end
 end
