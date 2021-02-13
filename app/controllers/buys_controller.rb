ad51a843311db12e1c047b528738764775d12da9
kodaikawabe <kokodai5124@icloud.com>
2021-02-13 12:15:49 +0900

    クレジットカード機能

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -10,6 +10,12 @@ class BuysController < ApplicationController
     @user_order = UserOrder.new(buy_params)
     if @user_order.valid?
       @user_order.save
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: buy_params[:token],
+        currency: 'jpy'
+      )
       redirect_to root_path
     else
       render action: :index
@@ -19,6 +25,6 @@ class BuysController < ApplicationController
   private
   def buy_params
     params.require(:user_order).permit(:postal_code, :prefectures_id, :municipal_district,:address,
-      :building_name).merge( user_id: current_user.id)
+      :building_name, :phone_number).merge(user_id: current_user.id,item_id: params[:item_id],token: params[:token])
  end
 end
