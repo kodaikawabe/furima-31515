7491ec72499f7080f98be046f9364824d54ce080
kodaikawabe <kokodai5124@icloud.com>
2021-02-15 16:01:22 +0900

    単体テスト修正

--- spec/factories/user_orders.rb
+++ spec/factories/user_orders.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     prefectures_id { 2 }
     municipal_district { '横浜市緑区' }
     address { '青山1-1-1' }
+    building_name { '柳ビル103'}
     phone_number { '09012345678' }
     token { 'tok_abcdefghijk00000000000000000' }
   end
