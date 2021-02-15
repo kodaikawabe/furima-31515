1a2f93cf6ce74f29c44180fcf55d56a5990f402d
kodaikawabe <kokodai5124@icloud.com>
2021-02-15 16:00:35 +0900

    コントローラー修正

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -12,12 +12,7 @@ class BuysController < ApplicationController
     @user_order = UserOrder.new(buy_params)
     if @user_order.valid?
       @user_order.save
-      Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: buy_params[:token],
-        currency: 'jpy'
-      )
+      pay_item
       redirect_to root_path
     else
       render action: :index
@@ -29,6 +24,15 @@ class BuysController < ApplicationController
   def buy_params
     params.require(:user_order).permit(:postal_code, :prefectures_id, :municipal_district, :address,
                                        :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: buy_params[:token],
+        currency: 'jpy'
+      )
   end
 
   def set_item
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :show, :new, :create]
+  before_action :soldout_item, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -57,4 +58,9 @@ class ItemsController < ApplicationController
   def move_to_index
     redirect_to action: :index unless current_user.id == @item.user_id
   end
+
+  def soldout_item
+    redirect_to root_path if @item.buy.present?
+  end
+
 end
