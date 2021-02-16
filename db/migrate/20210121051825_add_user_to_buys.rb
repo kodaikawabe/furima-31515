36237545579f8d8d2b0f7ac83ed6f0bfb29cce20
kodaikawabe <kokodai5124@icloud.com>
2021-02-16 17:51:10 +0900

    修正

--- db/migrate/20210121051825_add_user_to_buys.rb
+++ db/migrate/20210121051825_add_user_to_buys.rb
@@ -1,5 +1,4 @@
 class AddUserToBuys < ActiveRecord::Migration[6.0]
   def change
-    add_reference :buys, :user, null: false, foreign_key: true
   end
 end
--- db/migrate/20210121051927_add_item_to_buys.rb
+++ db/migrate/20210121051927_add_item_to_buys.rb
@@ -1,5 +1,4 @@
 class AddItemToBuys < ActiveRecord::Migration[6.0]
   def change
-    add_reference :buys, :item, null: false, foreign_key: true
   end
 end
